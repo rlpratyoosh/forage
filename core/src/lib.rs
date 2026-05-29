5ff631a5eed7a355e6dd833cc8ad49e63cf13a7a
rlpratyoosh <pratyoosh.prakash.dev@gmail.com>
2026-05-29 11:21:10 +0530

    feat: add dynamic map size calculation and generation

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -92,22 +92,38 @@ impl NestPool {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct Settings {
-    player_count: usize,
-    ants_per_nest: usize,
     map_area: usize,
+    player_count: u32,
+    ants_per_nest: u32,
+    no_of_chunks: u32,
+    chunks_per_player: u16,
+}
+
+fn prev_power_of_two(n: usize) -> usize {
+    if n == 0 {
+        0
+    } else {
+        1 << (usize::BITS - n.leading_zeros() - 1)
+    }
 }
 
 impl Settings {
-    pub fn new(player_count: usize, ants_per_nest: usize, ant_density: f32) -> Self {
-        let required_area = (player_count * ants_per_nest) as f32 / ant_density;
+    pub fn new(player_count: usize, ants_per_nest: u32, ant_density: f32) -> Self {
+        let required_area = (player_count * ants_per_nest as usize) as f32 / ant_density;
         let width = (required_area.sqrt() as usize).next_power_of_two();
         let map_area = width * width;
+        let no_of_chunks = (map_area / 1024) as u32;
+        let chunks_per_player = prev_power_of_two((no_of_chunks as usize / player_count).isqrt()).pow(2) as u16;
+        let player_count = no_of_chunks / chunks_per_player as u32;
 
         Self {
             player_count,
             ants_per_nest,
             map_area,
+            no_of_chunks,
+            chunks_per_player,
         }
     }
 }
@@ -123,10 +139,10 @@ pub struct World {
 impl World {
     pub fn new(settings: Settings) -> Self {
         Self {
-            ant_pool: AntPool::new(settings.player_count, settings.ants_per_nest),
+            ant_pool: AntPool::new(settings.player_count as usize, settings.ants_per_nest as usize),
             food_pool: FoodPool::new(settings.map_area),
             pheromone_pool: PheromonePool::new(settings.map_area),
-            nest_pool: NestPool::new(settings.player_count, settings.map_area),
+            nest_pool: NestPool::new(settings.player_count as usize, settings.map_area as usize),
             settings,
         }
     }
@@ -136,4 +152,13 @@ impl World {
 mod tests {
     use super::*;
 
+    #[test]
+    fn settings() {
+        let settings = Settings::new(500, 500, 0.05);
+        assert_eq!(settings, Settings { player_count: 1024, ants_per_nest: 500, map_area: 16_777_216, no_of_chunks: 16_384, chunks_per_player: 16 } );
+        let settings = Settings::new(2000, 1000, 0.05);
+        assert_eq!(settings, Settings { player_count: 4096, ants_per_nest: 1000, map_area: 67_108_864, no_of_chunks: 65_536, chunks_per_player: 16 } );
+        let settings = Settings::new(1000, 500, 0.05);
+        assert_eq!(settings, Settings { player_count: 1024, ants_per_nest: 500, map_area: 16_777_216, no_of_chunks: 16_384, chunks_per_player: 16 } );
+    }
 }
